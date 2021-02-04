71158d6581e858dce77277a5f47529d77614eb2e
Clea Parcerisas <cleap@vliz.be>
2021-02-04 15:24:04 +0100

    added init MTE

--- pyhydrophone/__init__.py
+++ pyhydrophone/__init__.py
@@ -2,3 +2,4 @@ from pyhydrophone.seiche import Seiche
 from pyhydrophone.amar import AmarG3, AmarG3MEMS
 from pyhydrophone.soundtrap import SoundTrap, SoundTrapHF
 from pyhydrophone.bruelkjaer import BruelKjaer
+from pyhydrophone.mte import MTE
