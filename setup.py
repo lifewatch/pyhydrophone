3ec5e53777dd51d18e4b035be53c3a5634fa8602
Clea Parcerisas <cleaparcerisas@gmail.com>
2023-07-19 12:09:25 +0200

    changed to version 0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyhydrophone',
-      version='0.1.4',
+      version='0.1.5',
       description='Python scripts to read hydrophones files',
       url='https://github.com/lifewatch/pyhydrophone.git',
       author='Clea Parcerisas',
